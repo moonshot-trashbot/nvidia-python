e81148be8325c2c0fe77e9d484124040f22888ba
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-11 16:27:43 -0700

    Go

--- runtime.py
+++ runtime.py
@@ -1,4 +1,16 @@
 import json
 
+def modify(x):
+    if(hasattr(x, "TrackId") == False): x["TrackId"] = -1;
+    return {
+        "id": x["TrackID"],
+        "left": x["Left"],
+        "right": x["Right"],
+        "top": x["Top"],
+        "bottom": x["Bottom"],
+        "center": [x["Center"][0], x["Center"][1]]
+    }
+
 def go(detections):
-    print(json.dumps(detections))
+    for x in detections:
+        print(modify(x))
