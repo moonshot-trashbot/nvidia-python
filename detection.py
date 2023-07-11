d8f6eef6396accb11610c076fe99b58bcde13bed
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-11 11:55:46 -0700

    Create detection.py

---
+++ detection.py
@@ -0,0 +1,16 @@
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
+
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource("/dev/video0")
+display = videoOutput("display://0")
+
+while display.IsStreaming():
+    img = camera.Capture()
+
+    if img is None:
+        continue
+
+    detections = net.Detect(img)
+
+    print(detections)
