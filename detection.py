b7ed2810a6a92d4c6aa31bb3c226d720122b1354
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-11 16:52:46 -0700

    go

--- detection.py
+++ detection.py
@@ -6,14 +6,13 @@ import runtime
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
 net.SetTrackingEnabled(True)
 net.SetTrackingParams(minFrames=3, dropFrames=15, overlapThreshold=0.5)
-display = videoOutput("display://0")
-
 camera = videoSource("/dev/video0")
+display = videoOutput("display://0")
 
 while True:
     img = camera.Capture()
     if img is None: continue
-    display.render(img)
     detections = net.Detect(img)
+    display.render(img)
     runtime.go(detections)
     time.sleep(0.33)
