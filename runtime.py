342b00598817a197c66543e709cac0b8a3987e02
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-16 17:16:35 -0700

    hehe

--- runtime.py
+++ runtime.py
@@ -16,18 +16,27 @@ def modify(x, frame):
         "frame": frame
     })
 
-ss = socket.socket()
-ss.connect(("192.168.12.238", 420))
+socklist = []
 
 def go(detections):
+    global socklist
     frame = int(time.time())
+    ss = socket.socket()
+    socklist.append(ss)
+    ss.connect(("127.0.0.1", 420))
     for x in detections:
         mod = modify(x, frame)
         js = mod.toJSON()
         print(js)
         print(">>> Sending", js)
-        js = js.encode('utf-8') + "\\n".encode('utf-8')
-        ss.sendall(js)
+        js = js.encode('utf-8') + b"\n"
+        try:
+            ss.sendall(js)
+        finally:
+            ss.detach()
+            socklist.remove(ss)
 
 def complete():
-    ss.close()
+    global socklist
+    for x in socklist:
+        x.close()
