1bbcb04d96eb11d9c71ebf3f8c510f06c1ad94ac
RaspberryPi <annabellaproctor@gmail.com>
2023-07-19 15:21:23 -0700

    yo

--- detection.py
+++ detection.py
@@ -13,6 +13,7 @@ while True:
     img = camera.Capture()
     if img is None: continue
     detections = net.Detect(img)
-    # display.Render(img) 
+#   display.Render(img) 
     runtime.go(detections)
     time.sleep(1)
+
